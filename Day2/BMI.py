61beab27a6a3d2c772405d50f686a4346400107d
Ayesha Kanwal <108638968+Ay3-shA@users.noreply.github.com>
2024-07-11 23:10:32 +0000

    Body Mass Index

---
+++ Day2/BMI.py
@@ -0,0 +1,10 @@
+#body mass index 
+
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in kg: ")
+
+BMI = int (weight) / float (height) ** 2
+
+print (int(BMI))
+
+#----------------------------------------------
