5f8a46304dd11685e584c4814d578ffc4e6fe85e
Ayesha Kanwal <108638968+Ay3-shA@users.noreply.github.com>
2024-07-11 19:44:35 +0000

    add two digit number

---
+++ Day2/add_two_digit_num.py
@@ -0,0 +1,9 @@
+#add two digit number 35 (3 + 5 = 8)
+
+number = input("Write two digit number: ")
+num1 = int(number[0])
+num2 = int(number[1])
+sum = (num1 + num2)
+print ("sum of two digit number is : " + str(sum))
+
+#--------------------------------------------------
