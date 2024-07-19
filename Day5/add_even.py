abdacf6e2d22142ff647b8d2697363408d8392ab
Ayesha Kanwal <108638968+Ay3-shA@users.noreply.github.com>
2024-07-19 11:17:13 +0000

    add even numbers

---
+++ Day5/add_even.py
@@ -0,0 +1,9 @@
+#Adding even numbers 
+
+even_sum = 0
+for numbers in range(2, 101, 2):
+  #print(numbers)
+  even_sum += numbers
+print(f"the sum of the even numbers is {even_sum}")
+
+#--------------------------------------------------------
