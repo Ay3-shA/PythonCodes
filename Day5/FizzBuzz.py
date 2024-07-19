fa4d1340204a603ca571465d2c6cf1a452c20871
Ayesha Kanwal <108638968+Ay3-shA@users.noreply.github.com>
2024-07-19 11:40:03 +0000

    FizzBuzz

---
+++ Day5/FizzBuzz.py
@@ -0,0 +1,17 @@
+# FizzBuzz 
+
+for number in range(1, 101):
+
+  if int(number) % 3 == 0 and int(number) % 5 == 0:
+    print("FizzBuzz")
+  elif int(number) % 5 == 0:
+    print("Buzz")
+  elif int(number) % 3 == 0:
+    print("Fizz")
+  else:
+    print(number)
+
+#--------------------------------------------------------
+   
+    
+    
