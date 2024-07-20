125a8301177fa0e3bebf2a2486df6568c94e07d4
Ayesha Kanwal <108638968+Ay3-shA@users.noreply.github.com>
2024-07-20 21:49:59 +0000

    hangman game

---
+++ Day7/hangman.py
@@ -0,0 +1,27 @@
+#Hangman
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+  display += "_"
+print (display)
+  
+
+guess = input("Guess a letter.\n").lower()
+
+for position in range(word_length):
+  letter = chosen_word[position]
+  if letter == guess:
+    display[position] = letter
+print(display)
+
+    
+    
+
+
