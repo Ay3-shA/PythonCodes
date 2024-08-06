960792618feee5d8dfad2b93b41422d9edc8b4c3
Ayesha Kanwal <108638968+Ay3-shA@users.noreply.github.com>
2024-08-06 11:27:32 +0000

    correct

--- Day8/prime_number.py
+++ Day8/prime_number.py
@@ -3,10 +3,17 @@
 
 def prime_checker(number):
 
-  if number % 2 == 1 and number % 3 != 0:
-        print("It's a prime number.")
-  else:
+  if number <=1:
     print("It's not a prime number.")
+    return
+  
+  for i in range(2, number):
+    if number % i == 0:
+      print("It's not a prime number.")
+      return
+  
+  print("It's a prime number.")
+  
 
 
 
